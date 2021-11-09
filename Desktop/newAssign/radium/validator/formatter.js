f4ab0f5b26144af80173866fadbeff3d3b437084
dhi7050 <dhirajkumarr579@gmail.com>
2021-11-09 11:39:28 +0530

    module system assignment

---
+++ Desktop/newAssign/radium/validator/formatter.js
@@ -0,0 +1,15 @@
+function trim(n){
+console.log(n.trim());
+}
+
+function changeToUpperCase(str){
+console.log(str.toUpperCase());
+}
+
+function changeToLowerCase(str1){
+console.log(str1.toLowerCase());
+}
+
+module.exports.myTrim = trim;
+module.exports.changeToUpperCase = changeToUpperCase;
+module.exports.changeToLowerCase = changeToLowerCase;
