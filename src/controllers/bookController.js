1567c6deebec51cfbf60d7775a016e73ba4ae2a0
dhi7050 <dhirajkumarr579@gmail.com>
2021-11-18 16:42:20 +0530

    refPopulate assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,14 +1,24 @@
 const bookModel = require("../models/bookModel.js");
+const AuthorModel = require("../models/authorModel")
 const mongoose = require("mongoose");
 
 const createBook = async function (req, res) {
-  const book = req.body;
-  let savedBook = await bookModel.create(book);
-  res.send({ msg: savedBook });
-};
+  let body = req.body;
+  let authorId = req.body.author;
+  //console.log(authorId)
+  let savedBook = await AuthorModel.findById(authorId);
+ console.log(savedBook)
+  if(savedBook){
+    let createbook = await bookModel.create(body);
+    res.send({ msg: createbook });
+  }else{
+    res.send("not valid")
+  }
+  
+}; 
 
 const getBooks = async function (req, res) {
-  let allBooks = await bookModel.find().populate('author');
+  let allBooks = await bookModel.find().populate('author') 
   res.send({ msg: allBooks });
 };
 
